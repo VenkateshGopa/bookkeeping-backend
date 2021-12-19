0882c0472ecc4d4235f30d9f813fca5a68a45cd2
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-12-19 11:40:58 +0530

    Update bookkeeping.js

--- Routes/bookkeeping.js
+++ Routes/bookkeeping.js
@@ -5,8 +5,8 @@ const route = express.Router();
 route.get ('/profile', services.profile)
 route.post('/newbook' , services.newbook);
 route.get('/books', services.getbooks);
-route.post('/changepassword', services.changepassword)
-// route.post('/newcustomer', services.login);
-// route.post('/newentry' , services.activation);
+route.post('/changepassword', services.changepassword);
+route.get('/delete/:id' , services.deletedata);
+
 
 module.exports= route;
