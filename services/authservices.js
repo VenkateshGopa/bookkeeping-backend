119d89e9cc3c62c92bb8536c716a045ff500d434
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-12-20 08:09:01 +0530

    Update authservices.js

--- services/authservices.js
+++ services/authservices.js
@@ -32,7 +32,7 @@ const services = {
       await email(value.email , `<h2>Hello ${value.firstname} ${value.lastname},</h2> <p> Book keeping account has been created Successfully. The below Link to Activate your book keeping account.</p>  
       <span><a href="https://nervous-poitras-54f763.netlify.app/active/${registereduser.insertedId}">Click here</a> to activate your account</span>
       <P>(or) Use the below link</p>
-      <p>https://nervous-poitras-54f763.netlify.app/active/${registereduser.insertedId}</p>`, "Activate yout book keeping Account");
+      <p>https://nervous-poitras-54f763.netlify.app/active/${registereduser.insertedId}</p>`, <p>Activate yout book keeping Account</p>);
         res.status(201).send({message:"registered Succesfully"});
     }
     catch(error){
@@ -100,7 +100,7 @@ const services = {
       await email(value.email , `<h2>Hello ${user.firstname} ${user.lastname},</h2> <p> A request has been received to change the password for your account. The below Link to reset your account password is only valid for 30Minutes.</p>  
       <span><a href="https://nervous-poitras-54f763.netlify.app/${user._id}/${code}">Click here</a> to reset your password</span>
       <P>(or) Use the below link</p>
-      <p>https://nervous-poitras-54f763.netlify.app/${user._id}/${code}</p>`, "Reset password ");
+      <p>https://nervous-poitras-54f763.netlify.app/${user._id}/${code}</p>`, <p>Reset password </p>);
 //       await email(value.email , `https://nervous-poitras-54f763.netlify.app/${user._id}/${code}`);
       await mongo.db.collection('userdetails').updateOne({email:value.email} , {$set:{code: code, time:(value.time +1800000)} })
       res.send({message:"email send successfully"})
